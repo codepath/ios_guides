541f390870b7d73c1c2a4d5442ba92107c5fc49c
Ben Sandofsky <bsandofsky@twitter.com>
2014-10-15 00:00:32 -0700

    Add photos framework demo.

---
+++ SystemFrameworksDemo/SystemFrameworksDemo/PhotosViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PhotosViewController.swift
+//  SystemFrameworksDemo
+//
+//  Created by Ben Sandofsky on 10/14/14.
+//  Copyright (c) 2014 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBAction func didTapLoadFromLibrary(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary){
+            let picker = UIImagePickerController()
+            picker.sourceType = .PhotoLibrary
+            picker.delegate = self
+            self.presentViewController(picker, animated: true) { }
+        } else {
+            NSLog("Photo Library unavailable.")
+        }
+    }
+
+    @IBAction func didTapLoadFromCamera(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = .Camera
+            picker.delegate = self
+            self.presentViewController(picker, animated: true) { }
+        } else {
+            NSLog("Camera unavailable.")
+        }
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        self.imageView.image = image
+        self.dismissViewControllerAnimated(true) {}
+    }
+}

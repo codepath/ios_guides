d844074d3797bb6b4464f03579cb34f2c5cd132f
Ben Sandofsky <bsandofsky@twitter.com>
2014-10-15 02:33:34 -0700

    Add accelerometer example.

---
+++ SystemFrameworksDemo/SystemFrameworksDemo/MotionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MotionViewController.swift
+//  SystemFrameworksDemo
+//
+//  Created by Ben Sandofsky on 10/15/14.
+//  Copyright (c) 2014 CodePath. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class MotionViewController: UIViewController {
+    lazy var motionManager:CMMotionManager = CMMotionManager()
+
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (accelerometerData, error) in
+            let a = accelerometerData.acceleration
+            var sum = (a.x * a.x) + (a.y * a.y) + (a.z * a.z)
+            let w = self.view.bounds.size.width
+            self.widthConstraint.constant = w - (w * CGFloat(sum / 3.0))
+            NSLog("Data: \(sqrt(sum))")
+            return ()
+        })
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.motionManager.stopAccelerometerUpdates()
+    }
+}

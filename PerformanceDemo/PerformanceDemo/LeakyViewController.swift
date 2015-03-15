ff8fd3318e6ea76bc2f8bded3d1f01a6ed619862
Ben Sandofsky <ben@sandofsky.com>
2015-03-15 16:07:42 -0700

    Add an example of a block retain cycle.

--- PerformanceDemo/PerformanceDemo/LeakyViewController.swift
+++ PerformanceDemo/PerformanceDemo/LeakyViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class LeakyViewController: UIViewController {
     var leakyObject:LeakyObject = LeakyObject()
+
+    @IBOutlet weak var keyboardStateLabel: UILabel!
+    var keyboardObserverOrNil:AnyObject?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.keyboardStateLabel.hidden = true
+        keyboardObserverOrNil = NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardWillShowNotification, object: nil, queue: NSOperationQueue.mainQueue()){ notification in
+            self.keyboardStateLabel.hidden = false
+        }
+    }
+
+    deinit {
+        if let keyboardObserver:AnyObject = keyboardObserverOrNil {
+            NSNotificationCenter.defaultCenter().removeObserver(keyboardObserver)
+        }
+        NSLog("View controller deinitialized")
+    }
 }
 
 class LeakyObject {

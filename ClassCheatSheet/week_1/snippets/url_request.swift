9aee70085e38230edf971fb9248c555cf5c63bd1
Benjamin Sandofsky <ben@sandofsky.com>
2015-09-07 23:54:42 -0700

    Update to use NSURLSession.

--- ClassCheatSheet/week_1/snippets/url_request.swift
+++ ClassCheatSheet/week_1/snippets/url_request.swift
@@ -1,9 +1,10 @@
 let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=<#ApiKey#>"
 let request = NSMutableURLRequest(URL: NSURL(string:RottenTomatoesURLString)!)
-NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
-    if let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary? {
+let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+    if let dictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
         NSLog("Dictionary: \(dictionary)")
     } else {
-
+        
     }
-})
+}
+task.resume()

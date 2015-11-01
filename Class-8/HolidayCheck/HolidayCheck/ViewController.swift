1d82e7d4fb6aa5405117929192028c5032b4878e
Benjamin Sandofsky <ben@sandofsky.com>
2015-11-01 11:15:08 -0800

    Initial checkin of HolidayCheck

---
+++ Class-8/HolidayCheck/HolidayCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolidayCheck
+//
+//  Created by Benjamin Sandofsky on 11/1/15.
+//  Copyright © 2015 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
